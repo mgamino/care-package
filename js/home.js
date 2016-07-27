510db32183ffdede38361534bef2fcc0fbb66966
Demo User <demouser@MacBook-Air-89.local>
2016-07-27 15:42:54 -0700

    sntop

--- js/home.js
+++ js/home.js
@@ -1,29 +1,3 @@
-function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(38.0000,-97.0000),
-    zoom:3,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-  for (var i = 0; i<60;i+=20){
-      var myCenter =new google.maps.LatLng(37.4825,-122.2364+i);
-      var image = 'images/gift-24.png'
-      var marker=new google.maps.Marker({
-      position:myCenter,
-      icon: image
-      });
-      marker.setMap(map);
-  }
-  // var infowindow = new google.maps.InfoWindow({
-  //   content:"Hello World!"
-  // });
-  //
-  // google.maps.event.addListener(this, 'click', function() {
-  //   infowindow.open(map,marker);
-  // });
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-
 function setup(){
 
   $(".dropdown1").hover(
@@ -47,5 +21,16 @@ function setup(){
     }
   );
 }
-
 $(document).ready(setup);
+
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(38.0000,-97.0000),
+    zoom:3,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position.coords.latitude, position.coords.longitude);
+  });
+}
