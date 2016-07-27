3ffb4d7e69f0e47ce3ea453d301e0faefc10eef9
Demo User <demouser@MacBook-Air-89.local>
2016-07-27 16:23:49 -0700

    yes

--- js/home.js
+++ js/home.js
@@ -23,14 +23,38 @@ function setup(){
 }
 $(document).ready(setup);
 
+// function initialize() {
+//   var mapProp = {
+//     center:new google.maps.LatLng(38.0000,-97.0000),
+//     zoom:3,
+//     mapTypeId:google.maps.MapTypeId.ROADMAP
+//   };
+//   var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+// }
+
+var geocoder;
+var map;
 function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(38.0000,-97.0000),
-    zoom:3,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-  navigator.geolocation.getCurrentPosition(function(position) {
-    console.log(position.coords.latitude, position.coords.longitude);
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var mapOptions = {
+    zoom: 8,
+    center: latlng
+  }
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
+function codeAddress() {
+  var address = document.getElementById('address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
   });
 }
