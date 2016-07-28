eef47ac35b1d823e908aa10d6fb5b04adbf09e84
Juan Ayala <ayala.juan40@gmail.com>
2016-07-28 07:53:14 -0700

    plz

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class MainHandler(webapp2.RequestHandler):
             email = user.email().lower()
             logout_url=users.CreateLogoutURL('/')
 
-            Letter.query(Letter.sender_email == email).fetch()#juan did this
+            letter = Letter.query(Letter.sender_email == email).fetch()#juan did this
 
 
             template = jinja_environment.get_template("home.html")
