cdc0c861c2899b6e522a5be9c66ef6881da96579
Juan Ayala <ayala.juan40@gmail.com>
2016-07-28 16:05:03 -0700

    work

--- js/home.js
+++ js/home.js
@@ -44,7 +44,6 @@ function codeAddress() {
     geocoder.geocode( { 'address': address},
       function(results, status) {
         if (status == 'OK') {
-          map.setCenter(results[0].geometry.location);
           var marker = new google.maps.Marker({
               map: map,
               position: results[0].geometry.location
